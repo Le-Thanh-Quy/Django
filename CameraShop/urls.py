86e358e06435ca36c43b7d9393baf608a87af92d
Le-Thanh-Quy <lethanhquy123321@gmail.com>
2022-06-11 17:29:59 +0700

    [Done] detail page

--- CameraShop/urls.py
+++ CameraShop/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:content>/search', views.search, name='search'),
+    path('<int:id_camera>/camera', views.detail_camera, name='detail_camera'),
+    path('<int:id_len>/len', views.detail_len, name='detail_len'),
 ]
--- CameraShop/views.py
+++ CameraShop/views.py
@@ -18,13 +18,15 @@ def index(request):
     list_len = sorted(list_len, key=lambda x: x.getStatus()[0], reverse=False)
 
     list_slides = [x for x in list_camera if x.getStatus()[0] == 0 or x.getStatus()[0] == 1]
+    slide_center_index = len(list_slides)
     list_slides = list_slides + [x for x in list_len if x.getStatus()[0] == 0 or x.getStatus()[0] == 1]
 
     context = {
         'list_camera': list_camera,
         'list_len': list_len,
         'list_slides': list_slides,
-        'is_search': False
+        'is_search': False,
+        'slide_center_index': slide_center_index
     }
     return render(request, 'CameraShop/view/index.html', context)
 
@@ -39,6 +41,7 @@ def search(request, content):
     list_len = [x for x in list_len if x.name.upper().find(str(content).upper()) != -1]
 
     list_slides = [x for x in list_camera if x.getStatus()[0] == 0 or x.getStatus()[0] == 1]
+    slide_center_index = len(list_slides)
     list_slides = list_slides + [x for x in list_len if x.getStatus()[0] == 0 or x.getStatus()[0] == 1]
 
     context = {
@@ -46,6 +49,45 @@ def search(request, content):
         'list_len': list_len,
         'list_slides': list_slides,
         'is_search': True,
-        'content_search': content
+        'content_search': content,
+        'slide_center_index': slide_center_index
     }
     return render(request, 'CameraShop/view/index.html', context)
+
+
+def detail_camera(request, id_camera):
+    product = Camera.objects.get(id=id_camera)
+
+    list_camera = Camera.objects.all().order_by("-id")
+    list_camera = sorted(list_camera, key=lambda x: x.getStatus()[0], reverse=False)
+    list_camera = [x for x in list_camera if str(x.lensMount).upper().find(str(product.lensMount).upper()) != -1]
+
+    list_len = Lens.objects.all().order_by("-id")
+    list_len = sorted(list_len, key=lambda x: x.getStatus()[0], reverse=False)
+    list_len = [x for x in list_len if str(x.lensMount).upper().find(str(product.lensMount).upper()) != -1]
+
+    context = {
+        'camera': product,
+        'list_camera': list_camera,
+        'list_len': list_len,
+    }
+    return render(request, 'CameraShop/view/detail.html', context)
+
+
+def detail_len(request, id_len):
+    product = Lens.objects.get(id=id_len)
+
+    list_camera = Camera.objects.all().order_by("-id")
+    list_camera = sorted(list_camera, key=lambda x: x.getStatus()[0], reverse=False)
+    list_camera = [x for x in list_camera if str(x.lensMount).upper().find(str(product.lensMount).upper()) != -1]
+
+    list_len = Lens.objects.all().order_by("-id")
+    list_len = sorted(list_len, key=lambda x: x.getStatus()[0], reverse=False)
+    list_len = [x for x in list_len if str(x.lensMount).upper().find(str(product.lensMount).upper()) != -1]
+
+    context = {
+        'len': product,
+        'list_camera': list_camera,
+        'list_len': list_len,
+    }
+    return render(request, 'CameraShop/view/detail.html', context)
