af3ccdf7ab6b934c182932429458e3fc7cb9346c
Tran-Nguyen-Anh-Trinh <trinhtranthanhxuyen@gmail.com>
2022-06-16 16:16:05 +0700

    [102] UI Bill Page And History Page

--- CameraShop/urls.py
+++ CameraShop/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('order_page/<int:user_id>', views.order_page),
+    path('bill_page/<int:user_id>', views.bill_page),
+    path('history_page/<int:user_id>', views.history_page),
 ]
--- CameraShop/views.py
+++ CameraShop/views.py
@@ -29,3 +29,38 @@ def order_page(request, user_id):
                   {"list_all": list_all,
                    "list_index": list_index,
                    "json": json})
+
+def bill_page(request, user_id):
+    list_item = Camera.objects.all()[:5]
+    list_index = list(range(len(list_item)))
+    list_image = []
+    for item in list_item:
+        list_image.append(Image.objects.get(id = item.image_id).productFrontImage.url)
+
+    list_all = zip(list_index, list_item)
+
+    # for index, item in li :
+
+    json = '{"input_19_1001": {"price": "10", "quantityField": "input_19_quantity_1001_0", "custom_1": "input_19_custom_1001_1"}}'
+    return render(request, 'CameraShop/view/bill_page.html',
+                  {"list_all": list_all,
+                   "list_index": list_index,
+                   "json": json})
+
+
+def history_page(request, user_id):
+    list_item = Camera.objects.all()[:5]
+    list_index = list(range(len(list_item)))
+    list_image = []
+    for item in list_item:
+        list_image.append(Image.objects.get(id = item.image_id).productFrontImage.url)
+
+    list_all = zip(list_index, list_item)
+
+    # for index, item in li :
+
+    json = '{"input_19_1001": {"price": "10", "quantityField": "input_19_quantity_1001_0", "custom_1": "input_19_custom_1001_1"}}'
+    return render(request, 'CameraShop/view/history_page.html',
+                  {"list_all": list_all,
+                   "list_index": list_index,
+                   "json": json})
